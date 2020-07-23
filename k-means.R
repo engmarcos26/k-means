0ee6ecee2cfca6336d10b68d77af15002443b994
engmarcos26 <eng.marcos26@gmail.com>
2020-07-22 22:44:27 -0300

    Update k-means.R

--- k-means.R
+++ k-means.R
@@ -2,20 +2,20 @@
 
 #https://bookdown.org/wevsena/curso_r_tce/curso_r_tce.html
 #encurtador.com.br/FHJQ7
-#https://www.youtube.com/watch?v=0ImwDt2s_Sw   (ver vÌdeo em 5.47)
+#https://www.youtube.com/watch?v=0ImwDt2s_Sw   (ver v√≠deo em 5.47)
 #https://www.youtube.com/watch?v=6qleqPsrBqI
 
 
 #Chamar os pacotes
 #Fazer analise de cluster usando o k-means
 library(FactoMineR)
-#Plotar gr·fico
+#Plotar gr√°fico
 library(factoextra)
 #clara e pam
 library(cluster)
-#Pacote para importar dados em .csv(ver vÌdeo em 5.47)
+#Pacote para importar dados em .csv(ver v√≠deo em 5.47)
 library(readr)
-#	ManipulaÁ„o e transformaÁ„o de dados (str)
+#	Manipula√ß√£o e transforma√ß√£o de dados (str)
 library(dplyr)
 
 
@@ -24,7 +24,7 @@ library(dplyr)
 getwd()
 
 
-#Gera testes pseudos randÙmicos
+#Gera testes pseudos rand√¥micos
 set.seed(123)
 
 
@@ -41,7 +41,7 @@ df <- data.frame(dados_brutos)
 #Vizualizar os dados ou simplesmente ir em global enviromment
 View(df)
 
-#Visualizar a estrutura de dados(aqui verificar se os dados est„o ok)
+#Visualizar a estrutura de dados(aqui verificar se os dados est√£o ok)
 str(df)
 
 #Boxplot
@@ -49,20 +49,20 @@ boxplot(df$Altura)
 boxplot(df$Profundidade)
 boxplot(df$Largura)
 
-#Transformar em escala(PadronizaÁ„o dos dados)
+#Transformar em escala(Padroniza√ß√£o dos dados)
 dados <- scale(df)
 
 
 
 
-#Definir quantidade Ûtima de cluster
+#Definir quantidade √≥tima de cluster
 fviz_nbclust(dados, kmeans,method = "gap_stat")
 
 #Gerar o K-means com k=3
 dados_kmeans <- kmeans(dados,3, nstart = 10)
 
-#Visualizar o kmeans no gr·fico
-fviz_cluster(dados_kmeans, data = dados, main = "Agrupamento de Caixas        1 = MÈdia, 2 = Pequena e 3 = Grande",show.clust.cent=T, palette = "Set2", ggtheme = theme_minimal(),ellipse.type = "euclid",geom = "point")
+#Visualizar o kmeans no gr√°fico
+fviz_cluster(dados_kmeans, data = dados, main = "Agrupamento",show.clust.cent=T, palette = "Set2", ggtheme = theme_minimal(),ellipse.type = "euclid",geom = "point")
 
 #Criando uma lista com os clusters
 Lista_dos_clusters <- dados_kmeans$cluster
